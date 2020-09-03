309815f0cffdc498af1db8a0b1b054e9d16ee67b
Lucas Teixeira <lucasteixeira.ufmg@gmail.com>
2020-09-03 13:36:48 +0200

    Implemented test to create_lines

--- tests/unit/test_line.py
+++ tests/unit/test_line.py
@@ -1,4 +1,4 @@
-from mesh.line import ProblemSetup
+from mesh.line import ProblemSetup, Line
 
 
 def test_read_input_file():
@@ -9,3 +9,16 @@ def test_read_input_file():
     result = ps.read_input_file(input_file)
 
     assert result == expected
+
+def test_create_lines():
+    input_list = [[91, 179, 760, 353], [874, 890, 648, 114]]
+    line1 = Line((91, 179), (760, 353))
+    line2 = Line((874, 890), (648, 114))
+    ps = ProblemSetup()
+
+    expected = [line1, line2]
+    result = ps.create_lines(input_list)
+
+    for res, exp in zip(result, expected):
+        assert res.point1 == exp.point1
+        assert res.point2 == exp.point2
