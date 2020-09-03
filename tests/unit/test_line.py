33caa34de5939e1fb3559f8ab54bf6428cf8fafb
Lucas Teixeira <lucasteixeira.ufmg@gmail.com>
2020-09-03 12:10:48 +0200

    Fixed path to test file

--- tests/unit/test_line.py
+++ tests/unit/test_line.py
@@ -2,7 +2,7 @@ from mesh.line import ProblemSetup
 
 
 def test_read_input_file():
-    input_file = "../data/input_points.txt"
+    input_file = "..\\mesh\\tests\\data\\input_points.txt"
     expected = [[91, 179, 760, 353], [874, 890, 648, 114]]
 
     ps = ProblemSetup()
