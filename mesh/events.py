02faf3926735e03abc830e9e77f3dc9f7ae54e10
Lucas Teixeira <lucasteixeira.ufmg@gmail.com>
2020-09-07 23:15:33 +0200

    Event factory has a point as a parameter

--- mesh/events.py
+++ mesh/events.py
@@ -1,36 +1,46 @@
 """Module to implement the events and the events factory."""
 
 
-from abc import ABC, abstractmethod
+from abc import abstractmethod
+
+from mesh.line import Point
 
 
 class EventsFactory():
     """Concrete creator for events objects."""
 
     @staticmethod
-    def create_event(event_type: str):
+    def create_event(event_type: str, point: Point):
         """Factory method for event objects.
 
         Parameters
         ----------
         event_type : str
             Type of event.
+
+        point: Point
+            Point to be added as an event.
         """
 
         try:
             if event_type == "StartPoint":
-                return StartPoint()
+                return StartPoint(point)
             elif event_type == "EndPoint":
-                return EndPoint()
+                return EndPoint(point)
             elif event_type == "Intersection":
-                return Intersection()
+                return Intersection(point)
             raise AssertionError("Event type not found")
         except AssertionError as _error:
             print(_error)
 
 
-class Events(ABC):
+class Events():
     """Product interface for the events objects."""
+
+    point: Point
+
+    def __init__(self, point: Point):
+        self.point = point
 
     @abstractmethod
     def handle_event(self):
